5f4c72175d5058acab42d160f4d7a929184b6d54
magicthighs911 <82037522+magicthighs911@users.noreply.github.com>
2023-03-24 18:01:57 -0400

    add rsi to homepage

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -206,6 +206,83 @@ st.markdown(f''' ####  If you would have invested :green[$ *{amount:,.2f}*] in t
 #### it would be worth :green[$ *{previous_date_value:,.2f}*] today. :eyes: ''')
 st.markdown("""---""")
 
+#####################################################################################################################################################
+
+rsi_window = st.sidebar.slider('RSI Window', 1, 200, 14)
+
+
+# Define function to calculate RSI for a given DataFrame
+def get_rsi(data):
+    delta = data['Adj Close'].diff()
+    gain = delta.where(delta > 0, 0)
+    loss = -delta.where(delta < 0, 0)
+    avg_gain = gain.rolling(window=rsi_window).mean()
+    avg_loss = loss.rolling(window=rsi_window).mean()
+    rs = avg_gain / avg_loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
+
+# Get list of tickers from user input
+# Import tickers from SessionState
+if 'tickers' not in st.session_state:
+    st.warning('Please enter tickers on the previous page.')
+else:
+    tickers = st.session_state.tickers
+
+# Get start date from user input
+start_date = st.date_input("Start Date", dt.date(2021, 1, 1))
+
+# Create empty dataframe to store RSI data
+rsi_df = pd.DataFrame(columns=['Ticker', 'RSI'])
+
+# Loop through tickers and calculate RSI
+for ticker in tickers:
+    # Get historical price data
+    stock_data = yf.download(ticker, start=start_date, end=dt.datetime.now().strftime('%Y-%m-%d'))
+    # Calculate RSI
+    rsi = get_rsi(stock_data)
+    last_rsi = rsi.iloc[-1]
+
+    # Add RSI to dataframe
+    rsi_df = rsi_df.append({'Ticker': ticker, 'RSI': last_rsi}, ignore_index=True)
+
+###########################################################################################################################
+# Save tickers to SessionState
+if 'tickers' not in st.session_state:
+    st.session_state.tickers = rsi_df
+###########################################################################################################################
+
+# Display RSI dataframe at the top of the page
+st.write(rsi_df)
+
+# Loop through tickers and plot RSI
+for ticker in tickers:
+    # Get historical price data
+    stock_data = yf.download(ticker, start=start_date, end=dt.datetime.now().strftime('%Y-%m-%d'))
+    # Calculate RSI
+    rsi = get_rsi(stock_data)
+
+    # Define oversold and overbought RSI ranges
+    oversold = 30
+    overbought = 70
+
+    # Plot RSI over time using Plotly Express
+    figg = px.line(x=rsi.index, y=rsi.values, title=ticker + ' RSI')
+    figg.update_xaxes(title_text='Date')
+    figg.update_yaxes(title_text='RSI')
+
+    # Add lower boundary line for oversold RSI range
+    figg.add_hline(y=oversold, line_dash="dash", annotation_text="Oversold", 
+                  annotation_position="bottom right", line_color="red")
+
+    # Add upper boundary line for overbought RSI range
+    figg.add_hline(y=overbought, line_dash="dash", annotation_text="Overbought", 
+                  annotation_position="top right", line_color="red")
+
+st.plotly_chart(figg)
+
+#####################################################################################################################################################
+
 st.caption(':point_down: Check any of the boxes below to see more details about the portfolio.')
 
 results = st.checkbox('Detailed Results')
