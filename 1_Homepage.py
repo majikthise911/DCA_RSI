251122bf84de2213b89f134196f5df9ac5726329
magicthighs911 <82037522+magicthighs911@users.noreply.github.com>
2023-04-02 21:28:42 -0400

    removed default tickers

--- 1_Homepage.py
+++ 1_Homepage.py
@@ -53,7 +53,7 @@ st.markdown("""---""")
 st.markdown('''### 2. What?
 Enter assets you would like to test as a portfolio''')
 st.caption(''' Enter tickers separated by commas WITHOUT spaces, e.g. "TSLA,ETH-USD,BTC-USD,AVAX-USD,OCEAN-USD,DOT-USD,MATIC-USD" ''')
-tickers_string = st.text_input('Tickers', 'TSLA,ETH-USD,BTC-USD,AVAX-USD,OCEAN-USD,DOT-USD,MATIC-USD').upper()
+tickers_string = st.text_input('Tickers').upper()
 tickers = tickers_string.split(',')
 
 #################################################3/24/23##########################################################################
